d833dff6d1d1815c989dd08ec5d1ad96f7221ad0
Saumyaranjan <saumyaranjan.sp20@gmail.com>
2021-11-08 17:39:36 +0530

    starting with database

--- project/base/views.py
+++ project/base/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+rooms = [
+    {'id':1,'name':'sam'},
+    {'id':2,'name':'bam'}
+    ]
+
+books = [{id:1,'name':'sherlock'}]
+
+def home(request):
+    context = {'rooms':rooms}
+    return render(request,'base/home.html',context)
+
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+
+    return render(request,'base/room.html',context)
+ 
