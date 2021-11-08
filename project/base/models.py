92c2aae4c40d656b601d2cf8649095605c397ce2
Saumyaranjan <saumyaranjan.sp20@gmail.com>
2021-11-08 21:06:45 +0530

    database room

--- project/base/models.py
+++ project/base/models.py
@@ -1,3 +1,20 @@
+# Creating database table 
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    # host
+    # topic
+    name = models.CharField(max_length = 200)
+    description = models.TextField(null=True, blank=True)
+    # participants = 
+    # when first saved not everytime 
+    updated = models.DateTimeField(auto_now = True)  
+    #  snapshot on ever item we saved
+    created = models.DateTimeField(auto_now_add = True)
+
+
+    def __str__(self):
+        return self.name
+
+    
--- project/base/views.py
+++ project/base/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
-rooms = [
-    {'id':1,'name':'sam'},
-    {'id':2,'name':'bam'}
-    ]
+# rooms = [
+#     {'id':1,'name':'sam'},
+#     {'id':2,'name':'bam'}
+#     ]
 
-books = [{id:1,'name':'sherlock'}]
+# books = [{id:1,'name':'sherlock'}]
 
 def home(request):
+    # gets all the users of the room
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room':room}
 
     return render(request,'base/room.html',context)
