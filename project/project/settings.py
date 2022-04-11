76a7858167ca0dc1d1f2fd501c6698d38cf4bc72
m4249 <m42491398@gmail.com>
2022-04-11 23:02:38 +0000

    vdds

--- project/project/settings.py
+++ project/project/settings.py
@@ -137,6 +137,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # WHERE TO UPLOAD USER PROFILE PICTURE
 # if user gives photo then save it to static/images 
